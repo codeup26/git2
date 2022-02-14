79874db2b7eecf94fc9702173ab358875b492be0
codeup26 <86265604+codeup26@users.noreply.github.com>
2022-02-14 17:38:05 -0500

    Create rom.lua

---
+++ rom.lua
@@ -0,0 +1,14 @@
+print('----------------')
+print('     LUA OS')
+while true do
+local i = io.read()
+  if i == 'git'
+    print('gitting packages')
+   print('git failed')
+    end
+  
+  if i == 'print' then
+    print('type here-------')
+    print(io.read("#")
+      end
+    end
